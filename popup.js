1e138edbf916f76f4fbbc4751fc1c9ce09df4041
Erik Huang <huangti@uw.edu>
2019-10-12 20:43:28 -0700

    autoloading

--- popup.js
+++
@@ -1,9 +0,0 @@
-let stop_button = document.getElementById('stop');
-
-stop_button.onclick = function(element) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'alert("Stopped")'});
-  });
-};
--- test.js
+++ test.js
@@ -1,4 +1,17 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log("success!!")
+    setTimeout("getPassword()", 1000);
 });
+
+function getPassword() {
+    $("input").each(function () {
+        let key = $(this).attr("type");
+        if(key == "password"){
+          console.log("Be careful when you put in password")
+        }else{
+          console.log("No password on this webpage")
+        }
+    });
+    console.log("Done!!")
+}
