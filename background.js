ca31c051c980761a81a3f66eda5341a7f4dd541d
Erik Huang <huangti@uw.edu>
2019-10-12 18:46:02 -0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,14 @@
+{
+  "name": "C-S-S",
+  "version": "1.0",
+  "description": "DubHacks let's go!!!",
+  "background": {
+    "scripts": ["background.js"],
+    "persistent": false
+  },
+  "manifest_version": 2
+  "permissions": ["storage"],
+  "page_action": {
+    "default_popup": "popup.html"
+  },
+}
